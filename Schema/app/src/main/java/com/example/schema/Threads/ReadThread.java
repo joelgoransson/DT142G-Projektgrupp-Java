5b59e5946a857531e73e3ff0fea0b8884b0d299b
LinneaJonsson <74564140+LinneaJonsson@users.noreply.github.com>
2020-12-12 20:04:05 +0100

    design fix

--- Schema/app/src/main/java/com/example/schema/Threads/ReadThread.java
+++ Schema/app/src/main/java/com/example/schema/Threads/ReadThread.java
@@ -12,7 +12,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class ReadThread implements Runnable {
-    private static final String BASE_URL="http://192.168.0.37:8080/Hemsida/webresources/";
+    private static final String BASE_URL="http://192.168.1.138:46185/Hemsida/webresources/";
     List<EmployeeObject> employeeList;
     @Override
     public void run() {
