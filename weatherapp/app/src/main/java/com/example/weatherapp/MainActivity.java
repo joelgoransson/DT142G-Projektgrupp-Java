82c3cd7363b71d062e3d0053e917ae412d964e4b
Afroanton <anton_zam@live.se>
2021-01-05 02:11:16 +0100

    fixade grafiska saker med väder appe

--- weatherapp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ weatherapp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.weatherapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,15 +34,52 @@ public class MainActivity extends AppCompatActivity {
 private RetrofitClient retrofitClient;
 private List<WeatherData> weatherDataList;
 private String weatherdata;
+private TextView tempTV;
+private TextView windTV;
+private TextView cloudTV;
+private TextView precipitationTV;
+private String tempData;
+private String windData;
+private String cloudData;
+private String precipitationData;
+private String weatherIconData;
+private ImageView weatherIcon;
+private Button refreshBtn;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String urlString = "https://api.met.no/weatherapi/locationforecast/2.0/classic?&lat=60.10&lon=9.58";
+        tempTV = findViewById(R.id.temp);
+        windTV = findViewById(R.id.wind);
+        cloudTV = findViewById(R.id.cloud);
+        precipitationTV = findViewById(R.id.precipitation);
+        weatherIcon = findViewById(R.id.weatherImage);
+        refreshBtn = findViewById(R.id.refreshButton);
+        refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWeatherData();
+                setWeatherData();
+            }
+        });
+
+    }
+    public void setWeatherData(){
+        tempTV.setText(tempData);
+        windTV.setText(windData);
+        cloudTV.setText(cloudData);
+        precipitationTV.setText(precipitationData);
+        int id = getResources().getIdentifier("drawable/" + weatherIconData,null,getPackageName());
+        weatherIcon.setImageResource(id);
+    }
+
+    public void getWeatherData(){
         Thread thread = new Thread(){
             public void run(){
+
                 System.out.println("Thread Running");
                 OkHttpClient client = new OkHttpClient();
 
@@ -64,25 +105,36 @@ private String weatherdata;
                     Document doc = builder.parse(src);
                     NamedNodeMap temperature = doc.getElementsByTagName("temperature").item(0).getAttributes();
                     NamedNodeMap windspeed = doc.getElementsByTagName("windSpeed").item(0).getAttributes();
+                    NamedNodeMap windDirection = doc.getElementsByTagName("windDirection").item(0).getAttributes();
                     NamedNodeMap cloudiness = doc.getElementsByTagName("cloudiness").item(0).getAttributes();
-                    NamedNodeMap precipitation = doc.getElementsByTagName("precipitation").item(0).getAttributes();
+                    NamedNodeMap precipitation = doc.getElementsByTagName("precipitation").item(1).getAttributes();
+                    NamedNodeMap symbol = doc.getElementsByTagName("symbol").item(0).getAttributes();
+
+                    String weatherIconId = symbol.item(0).getNodeValue();
+                    System.out.println(weatherIconId);
+
                     //tempvalues as Strings
-                    String tempcode = temperature.item(0).getNodeValue();
                     String temptype = temperature.item(1).getNodeValue();
                     String tempvalue = temperature.item(2).getNodeValue();
+
                     //Windvalues as Strings
-                    String windid = windspeed.item(0).getNodeValue();
                     String windmps = windspeed.item(1).getNodeValue();
-                    String windbeaufort = windspeed.item(2).getNodeValue();
-                    String windname = windspeed.item(3).getNodeValue();
+                    String windDirectionName = windDirection.item(2).getNodeValue();
+
                     //cloudvalues as Strings
-                    String cloudid = cloudiness.item(0).getNodeValue();
                     String cloudpercent = cloudiness.item(1).getNodeValue();
+
                     //raininfo as strings
                     String precipitationunit = precipitation.item(0).getNodeValue();
-                    String precipitationuvalue = precipitation.item(1).getNodeValue();
                     String precipitationmin = precipitation.item(2).getNodeValue();
                     String precipitationmax = precipitation.item(3).getNodeValue();
+
+                    //saves the weather data in class variables
+                    tempData = tempvalue + " " + temptype;
+                    windData = windmps + "mps, toward " + windDirectionName;
+                    cloudData = cloudpercent + "%";
+                    precipitationData = "Between " + precipitationmin + " " + precipitationunit + " and " + precipitationmax + " " + precipitationunit;
+                    weatherIconData = weatherIconId.toLowerCase();
 
                 } catch (ParserConfigurationException e) {
                     e.printStackTrace();
@@ -96,12 +148,16 @@ private String weatherdata;
             }
         };
         thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
 
 
 
-        //getWeatherData();
-
-    }
+    };
 
 
 
